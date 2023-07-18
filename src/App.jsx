aa0d98131369c867ceb948cf7bcf16a622433419
Rohit Tatwade <tatwaderohit369@gmail.com>
2023-07-18 13:07:57 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className='h-full w-full bg-red-200'>
+      <div className='h-screen w-full bg-red-900'>
+        1
+      </div>
+      <div className='h-screen w-full bg-red-800'>
+        2
+      </div>
+      <div className='h-screen w-full bg-orange-800'>
+        3
+      </div>
+      <div className='h-screen w-full bg-blue-800'>
+        4
+      </div>
+    </div>
+  )
+}
+
+export default App;
